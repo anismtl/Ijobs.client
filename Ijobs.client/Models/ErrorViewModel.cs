46950a841c71c83f9604f7d3b041a9cf2a563af6
Anis <aboutekadjiret@gmail.com>
2019-10-02 18:47:53 -0400

    Ajoutez des fichiers projet.

---
+++ Ijobs.client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ijobs.client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
